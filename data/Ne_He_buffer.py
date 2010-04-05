4bd4f1e9575337c0317a9323efcfc7e6f302652f
Gergely Imreh <imrehg@gmail.com>
2010-04-05 17:10:00 +0800

    update figures with ODR fit data

--- data/Ne_He_buffer.py
+++ data/Ne_He_buffer.py
@@ -1,44 +1,41 @@
 #!/usr/bin/env python
 
-#from numpy import *
 from pylab import *
 from scipy import *
-from scipy.optimize import *
+import scipy.odr as odr
 
 filename = 'Ne_He_buffer.CSV'
 data = loadtxt(filename, skiprows=1, delimiter=',', usecols=[0,1,2])
 
-
+# Scale frequencies
 freq = data[:,0] * 100
 fluo = data[:,1]
-fluostd = data[:,2]
+fluostd = data[:,2] 
 
 dd = zip(freq, fluo, fluostd)
 freq, fluo, fluostd = zip(*sorted(dd))
 
+# Gaussian fit function
 fitfunc = lambda p, x : p[2]*exp(-(x - p[0])**2 / (2*p[1]**2)) + p[3]
-errfunc = lambda p, x, y, err : (y - fitfunc(p, x))/err
-
-
 pin = [0, 40, -0.04,1]
-pout, cov, info, msg, success = leastsq(errfunc, pin[:], args=(freq,fluo,fluostd),full_output=1)
 
-print cov
-cerr = sqrt( cov[1][1] ) * 2*log(2)*sqrt(2)
-
-#popt, pcov = curve_fit(fitfunc, freq, fluo)
-#print popt, pcov
-
-#print pout
-
+# Actual fitting
+fitdata = odr.RealData(freq, fluo, sy=fluostd)
+model = odr.Model(fitfunc)
+fit = odr.ODR(fitdata, model, pin)
+fit.set_job(fit_type=2)
+output = fit.run()
+pout = output.beta
 
 b = pout[0]
 c = pout[1]
-x1 = -sqrt(-2*c**2*log(0.5))+b
-x2 = sqrt(-2*c**2*log(0.5))+b
-halfmax = pout[3]+pout[2]/2
+sd_c = output.sd_beta[1]
+fwhmscale = 2*sqrt(2*log(2))
+fwhm = c * fwhmscale
+fwhmsd = sd_c * fwhmscale
+halfmax = fitfunc(pout, b+fwhm/2.0)
 print "Fit contrast: %.1f%%" %(abs(pout[3]*pout[2])*100)
-print "FWHM: %.1f Hz" %(x2-x1)
+print "FWHM: %.0f(%.0f) Hz" %(fwhm, fwhmsd)
 
 
 fitfreq = linspace(freq[0]-10,freq[-1]+10)
@@ -63,14 +60,11 @@ rcParams.update(params)
 axes([0.125,0.2,0.95-0.125,0.95-0.2])
 errorbar(freq, fluo, fluostd, fmt='k.', linestyle='None', markersize=10)
 plot(fitfreq, fitfluo, 'k-', linewidth=2)
-plot([x1, x2],[halfmax, halfmax], 'k-.', linewidth=2)
-text(b, halfmax+0.005, "FWHM = %.0f(%.0f) Hz" %(x2-x1, cerr), horizontalalignment='center', verticalalignment='center', fontsize=15)
+plot([b-fwhm/2.0, b+fwhm/2.0],[halfmax, halfmax], 'k-.', linewidth=2)
+text(b, halfmax+0.005, "FWHM = %.0f(%.0f) Hz" %(fwhm, fwhmsd), horizontalalignment='center', verticalalignment='center', fontsize=15)
 xlim([fitfreq[0], fitfreq[-1]])
 xlabel('$100f_{rep} - f_{clock}$ (Hz)')
 ylabel('Fluorescence (a.u.)')
-#plot([freq[0], freq[-1]],[pout[3],pout[3]])
 
-#figure(2)
-#plot(freq,errfunc(pout, freq, fluo),'.')
 savefig('Ne_He_buffer.eps')
 show()
